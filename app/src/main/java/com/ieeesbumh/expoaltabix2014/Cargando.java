bcf57f8d02a928bdb9116576e750830379e4f4da
kudo <davutenjaca@gmail.com>
2014-11-12 01:44:45 +0100

    Corregido Splash a opción C: todos los mensajes, aleatorios

--- app/src/main/java/com/ieeesbumh/expoaltabix2014/Cargando.java
+++ app/src/main/java/com/ieeesbumh/expoaltabix2014/Cargando.java
@@ -5,9 +5,7 @@ package com.ieeesbumh.expoaltabix2014;
  */
 public class Cargando {
 
-    public String fraseCargando = new String("Cargando, pero con calma.."); // inicializada aunque no se usa
-    public int tiempoCargando;
-
+    // Se puede añadir cualquier nueva frase sin problemas
     public String[] listaFrases = {
             "Desencriptando todas tus contraseñas",
             "Instalando troyano en tu dispositivo",
@@ -15,22 +13,44 @@ public class Cargando {
             "Añadiendo una cucharada de azúcar",
             "Solucionando error NullKudoException"
             };
-    public int[] listaTiempos = {   //duracion de cada frase de listaFrases
-            1500,
+    // Son tiempos aleatorios, no hay relación directa entre esta lista y la de frases.
+    // Se van asignando al azar en el método 'getTiemposCargando'
+    public int[] listaTiempos = {
+            1000,
             1500,
             800,
             1300,
             1700
             };
 
-    public String setFraseCargando(int idFrase) {   //posiblemente redundante
-        fraseCargando = listaFrases[idFrase];
-        return fraseCargando;
+    // Arrays que recogerá las frases ya mezcladas
+    public String[] frasesCargando = new String[listaFrases.length];
+    // Array que recogerá un tiempo de duración para cada frase, por eso depende de 'listaFrases'
+    public int[] tiemposCargando = new int[listaFrases.length];
+
+    // Devuelve el array 'listaFrases' con sus frases desordenadas (rotadas)
+    public String[] getFrasesCargando(int idFraseInicial) {
+        // Rotamos el array para empezar en frase aleatoria
+        for (int i = 0; i < listaFrases.length; i++) {      // cuando llega ha cogido el último,
+            if (idFraseInicial+i == listaFrases.length) {   // vuelve al principio del array
+                idFraseInicial = 0 - i;    // asi vuelve al indice 0
+            }
+            frasesCargando[i] = listaFrases[idFraseInicial+i];
+        }
+
+        return frasesCargando;
     }
 
-    public int setTiempoCargando(int idFrase) {
-        tiempoCargando = listaTiempos[idFrase];
-        return tiempoCargando;
+    // Devuelve un array de tiempos totalmente aleatorio con valores sacados de 'listaTiempos'
+    public int[] getTiemposCargando(int idFraseInicial) {
+        // Aleatorizamos el array de tiempos (rotamos hacia atrás)
+        for (int i = 0; i < listaFrases.length; i++) {
+            if (idFraseInicial-i < 0) {                 // cuando llega al primero,
+                idFraseInicial = listaFrases.length;    // sigue hacia atrás desde el último
+            }
+            tiemposCargando[i] = listaTiempos[idFraseInicial-i];
+        }
+        return tiemposCargando;
     }
 
 }
